7b25321bf27f9cf1927867f60f340f6f20f621e8
devUbaid <devubaid493@gmail.com>
2025-04-29 18:59:28 +0530

    update

--- src/utils/axiosConfig.js
+++ src/utils/axiosConfig.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 // Create an axios instance with default config
 const api = axios.create({
-  baseURL: process.env.REACT_APP_API_URL || "http://localhost:5000",
+  baseURL: process.env.REACT_APP_API_URL || "http://localhost:5000" || "https://clientfeedbackportal-backend.onrender.com",
   headers: {
     "Content-Type": "application/json",
   },
